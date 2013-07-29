740146aaf1aa606705c1c718b38b3a15f959e5bd
Adam Solove <asolove@gmail.com>
2013-07-28 21:22:39 -0400

    Handle case where constraints aren't stable and blow the stack.

--- src/constrain/core.clj
+++ src/constrain/core.clj
@@ -39,12 +39,17 @@
         intercept (- start-energy (* slope start-value))]
     [intercept slope]))
 
-(defn walk-downhill [constraints var env]
-  (let [[intercept slope] (linear-approximation constraints var env)]
-    (if (< (abs slope) 0.1)
-      env
-      (recur constraints var (update-in env [var]
-                                        (if (neg? slope) - +)
-                                        0.1)))))
+(defn walk-downhill
+  ([constraints var env] (walk-downhill constraints var env 0))
+  ([constraints var env times]
+     (let [[intercept slope] (linear-approximation constraints var env)]
+       (cond
+        (> times 10000) (throw (Throwable. "Constraints aren't stable"))
+        (< (abs slope) 0.1) env
+        :else (recur
+               constraints
+               var
+               (update-in env [var] (if (neg? slope) - +) 0.1)
+               (inc times))))))
 
 
